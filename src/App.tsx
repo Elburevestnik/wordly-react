58339d047fe7fcd5b8937322b946a8cb00396309
= <=>
2022-10-12 00:11:09 +0300

    Add first version of logic for wordle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,36 @@
-import React, {useEffect, useState} from 'react';
+import React, {createContext, useEffect, useRef, useState} from 'react';
 import './App.css';
-import Words from './components/Words';
 import Table from './components/Table';
 
-const countOfLetter: number = 5;
-const countOfTrying: number = 6;
-let round: number = 0;
+export const WordleContext = createContext({
+    secretWord: '',
+    inputtedWord: '',
+    countOfLetter: 5,
+    countOfTrying: 6,
+});
 
 function App() {
-    const [words, setWords] = useState<string[]>([]);
-    const [selectedWord, setSelectedWord] = useState<string>('');
+    const [secretWord, setSecretWord] = useState<string>('world');
+    const [inputtedWord, setInputtedWord] = useState<string>('');
+    const inputRef = useRef<HTMLInputElement>(null);
+    const attemptNumber = useRef<number>(-1);
+    const [gameOvered, setGameOvered] = useState(false);
+    const inputWord = () => {
+        attemptNumber.current++;
+        const value = inputRef.current || {value: ''};
+        setInputtedWord(value.value);
+        inputRef.current && (inputRef.current.value = '');
+    };
 
+    useEffect(() => {
+        setSecretWord('world');
+    });
+
+    useEffect(() => {
+        if (inputtedWord === secretWord || countOfTrying === attemptNumber.current) {
+            setGameOvered(true);
+        }
+    }, [inputtedWord])
     // const initWord = async function f() {
     //     let wordList: string[] = [];
     //     if (!round) {
@@ -27,21 +47,20 @@ function App() {
 
     // initWord();
 
-    useEffect(() => {
-        const initWord = async function() {
-            let wordList: string[] = await Words(countOfLetter);
-            setWords(wordList);
-            setSelectedWord(wordList[round]);
-        }
-
-        initWord();
-    },[])
-
     return (
         <div className="App">
             <main className="App-header">
-                <Table word={selectedWord} countOfLetter={countOfLetter} countOfTrying={countOfTrying}/>
-                <button>Next Round</button>
+                <WordleContext.Provider value={{inputtedWord: inputtedWord, secretWord: secretWord, countOfLetter: countOfLetter, countOfTrying: countOfTrying}}>
+                    <Table attemptNumber={attemptNumber.current}/>
+                </WordleContext.Provider>
+                <input ref={inputRef} type="text" style={{marginTop: '10px'}} maxLength={countOfLetter} disabled={gameOvered}></input>
+                <button onClick={() => {inputWord()}} style={{marginTop: '10px'}} disabled={gameOvered}>Let's try</button>
+               <React.Fragment>
+                   {gameOvered && <div style={{height: '100px', width: '100px'}}>
+                        <p>The End!</p>
+                        <button onClick={() => (setGameOvered(false))}>Close</button>
+                    </div>}
+                </React.Fragment>
             </main>
         </div>
     );
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import {WordleContext} from '../App';
+
 export interface TableProps {
-    word: string;
-    countOfLetter: number;
-    countOfTrying: number;
+    attemptNumber: number;
 }
-export default function Table(props: TableProps) {
+export default function Table({attemptNumber}: TableProps) {
+    const {inputtedWord, secretWord, countOfLetter, countOfTrying} = useContext(WordleContext);
+
+    const [state, setState] = useState<Array<string[]>>([]);
+
+    useEffect(() => {
+        if(attemptNumber !== -1) {
+            inputtedWord.split('');
+            let newState = [...state];
+            newState[attemptNumber] = inputtedWord.split('');
+            setState(newState);
+        } else {
+            setState(Array(countOfTrying).fill(Array(countOfLetter).fill('')))
+        }
+    }, [inputtedWord, attemptNumber]);
+
     return (
         <table>
             <tbody>
-            {Array.from(Array(props.countOfTrying)).map((item: any, index: number) => {
-                return <tr key={index.toString()}>
-                    {Array.from(Array(props.countOfLetter)).map((item: any, idx: number) => {
-                        return <td key={index.toString() + idx.toString()}>{props.word[idx]}</td>
+            {state.map((item: any, index: number) => {
+                return (<tr key={index.toString()}>
+                    {item.map((item: any, idx: number) => {
+                        return (<td style={{
+                            border: '1px solid white',
+                            width: '30px',
+                            height: '30px',
+                            textAlign: 'center',
+                            verticalAlign: 'center',
+                            color: secretWord.includes(item) ? 'deepskyblue' : 'white',
+                        }} key={index.toString() + idx.toString()}>{item}</td>)
                     })}
-                </tr>;
+                </tr>);
             })}
             </tbody>
         </table>
